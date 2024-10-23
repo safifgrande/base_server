25cfe69b11e2e20a155cf750545dd8f8245c29ad
Fahmi Grande <fahmi.ayil@grandepos.io>
2024-10-23 14:27:24 +0700

    add values

--- functions/cobaAsync.js
+++
@@ -1,8 +0,0 @@
-module.exports = async () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Data fetched successfully');
-      // You can call reject('An error occurred') to simulate an error
-    }, 1000);
-  })
-}
