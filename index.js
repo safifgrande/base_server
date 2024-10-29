ec18002db77c2d27a4a7c17c99dd048ca505c165
Fahmi Grande <fahmi.ayil@grandepos.io>
2024-10-29 11:59:30 +0700

    fix routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("./engine/config")();
 require("./engine/logger")();
 const express = require("express");
-const { public_route, protected_route } = require("./routes");
 const BridgeMiddleware = require("./engine/middleware/bridgeMiddleware");
+const { routes } = require("./routes");
 
 const app = express();
 const port = 3000;
@@ -12,13 +12,19 @@ app.use(express.json());
 const bridge = new BridgeMiddleware();
 app.use((req, res, next) => bridge.initiate(req, res, next));
 
-public_route.forEach(({ path, method, handler }) => {
-  app[method](path, handler);
-});
+// PUBLIC API
+routes
+  .filter((route) => !route.middleware)
+  .forEach(({ path, method, handler }) => {
+    app[method](path, handler);
+  });
 
-protected_route.forEach(({ path, method, middleware, handler }) => {
-  app[method](path, middleware, handler);
-});
+// PRIVATE API
+routes
+  .filter((route) => route.middleware)
+  .forEach(({ path, method, middleware, handler }) => {
+    app[method](path, middleware, handler);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes.js
+++ routes.js
@@ -1,12 +1,4 @@
-const login = require("./api/v1/login");
-const clientUser = require("./functions/clientUser");
-const clientPaymentMedia = require("./functions/clientPaymentMedia");
-const adminEwallet = require("./functions/adminEwallet");
-const clientBankAccount = require("./functions/clientBankAccount");
-const clientBillDesign = require("./functions/clientBillDesign");
-const clientBillingHistory = require("./functions/clientBillingHistory");
-const adminRegisterClient = require("./functions/adminRegisterClient");
-const clientEwallet = require("./functions/clientEwallet");
+const pathConfig = require("./functions/config.json");
 
 const pathExtractor = (path, func, useMiddleware) => {
   const handler = async (req, res) => {
@@ -23,23 +15,21 @@ const pathExtractor = (path, func, useMiddleware) => {
   return basicPath;
 };
 
-const public_route = [
-  {
-    path: "/login",
+const routes = pathConfig.map((config) => {
+  if (config.private) {
+    return pathExtractor(
+      config.path,
+      require(`./functions/${config.handler}`),
+      config.private
+    );
+  }
+  return {
+    path: config.path,
     method: "post",
-    handler: login,
-  },
-];
+    handler: config.api
+      ? require(`./api/${config.handler}`)
+      : require(`./functions/${config.handler}`),
+  };
+});
 
-const protected_route = [
-  pathExtractor("/clientUser", clientUser, true),
-  pathExtractor("/clientPaymentMedia", clientPaymentMedia, true),
-  pathExtractor("/adminEwallet", adminEwallet, true),
-  pathExtractor("/clientBankAccount", clientBankAccount, true),
-  pathExtractor("/clientBillDesign", clientBillDesign, true),
-  pathExtractor("/clientBillingHistory", clientBillingHistory, true),
-  pathExtractor("/adminRegisterClient", adminRegisterClient, true),
-  pathExtractor("/clientEwallet", clientEwallet, true),
-];
-
-module.exports = { public_route, protected_route };
+module.exports = { routes };
