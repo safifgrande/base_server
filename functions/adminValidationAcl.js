42bbc707b0eaba598c17f2deb9a8d67ec6e2f3a9
esafif <esafif637@gmail.com>
2024-10-29 14:56:37 +0700

    init base

--- functions/adminValidationAcl.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (arg, list_acl) {
-  if (!arg || !list_acl) return false;
-
-  const aclExists = list_acl.find((v) => v === arg);
-  return aclExists ? true : false;
-};
--- functions/webHooks_whCallbackCreatedAccount.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (payload) {
-  return EJSON.parse(payload.body.text());
-};
