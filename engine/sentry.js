749600eafd0230c5e79e8c796d74f40d6b0e5d0a
Fahmi Grande <fahmi.ayil@grandepos.io>
2024-10-29 16:07:52 +0700

    environment sentry

--- engine/sentry.js
+++ engine/sentry.js
@@ -1,6 +1,6 @@
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "https://77b56635439772e5937fabf56f913232@sentry.mgc.pw/13",
+  dsn: process.env.SENTRY_URL,
   tracesSampleRate: 1.0,
 });
